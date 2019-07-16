3647c5c5f40700987750fc166c635639339fc267
msalers <mariasaleri@gmail.com>
2019-07-17 00:07:04 +0200

    Esempio slide

---
+++ cmake-build-release/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/maria/CLionProjects/Compiler/dynamicArray.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/dynamicArray.c.o"
+  "/home/maria/CLionProjects/Compiler/lexer.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/lexer.c.o"
+  "/home/maria/CLionProjects/Compiler/main.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/main.c.o"
+  "/home/maria/CLionProjects/Compiler/parser.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/parser.c.o"
+  "/home/maria/CLionProjects/Compiler/semantic.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/semantic.c.o"
+  "/home/maria/CLionProjects/Compiler/symbolTable.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/symbolTable.c.o"
+  "/home/maria/CLionProjects/Compiler/syntaxTree.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/syntaxTree.c.o"
+  "/home/maria/CLionProjects/Compiler/tcode.c" "/home/maria/CLionProjects/Compiler/cmake-build-release/CMakeFiles/main.dir/tcode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
